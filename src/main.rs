34f8253ae2cb8b6f325b51157cab8b189cdc5288
drkup <darrenkwondev46@gmail.com>
2026-03-05 07:43:58 +0900

    feat: 한국어 지원 폰트, 간단한 window 렌더

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+mod constants;
+
+use eframe::egui::{self, FontData, FontDefinitions, FontFamily};
+
+fn main() -> eframe::Result {
+    let rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_owned());
+
+    // SAFETY: we call this from the main thread without any other threads running.
+    #[expect(unsafe_code)]
+    unsafe {
+        std::env::set_var("RUST_LOG", rust_log);
+    };
+    env_logger::init();
+
+    // render eframe
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_title("krx-toolkit")
+            .with_inner_size([600.0, 60.0])
+            .with_resizable(false),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "krx-toolkit",
+        options,
+        Box::new(|cc| {
+            configure_fonts(&&cc.egui_ctx);
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {
+    _show_confirmation_dialog: bool,
+    _allowed_to_close: bool,
+    _always_on_top: bool,
+    _quick_input: String,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |_ui| {});
+    }
+}
+
+// ---------------------------
+fn configure_fonts(ctx: &egui::Context) {
+    let mut fonts = FontDefinitions::default();
+    fonts.font_data.insert(
+        "korean".to_owned(),
+        FontData::from_static(include_bytes!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/assets/fonts/PretendardVariable.ttf"
+        )))
+        .into(),
+    );
+    fonts
+        .families
+        .entry(FontFamily::Proportional)
+        .or_default()
+        .insert(0, "korean".to_owned());
+    fonts
+        .families
+        .entry(FontFamily::Monospace)
+        .or_default()
+        .insert(0, "korean".to_owned());
+    ctx.set_fonts(fonts);
 }
